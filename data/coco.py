eb7678eebff38aedf4de4262a23d54d4fec1e9df
AltTeter <200351p@ugs.kochi-tech.ac.jp>
2019-11-03 00:56:16 +0900

    クラスラベルをそのままの値で使用する

--- data/coco.py
+++ data/coco.py
@@ -62,7 +62,7 @@ class COCOAnnotationTransform(object):
                 bbox = obj['bbox']
                 bbox[2] += bbox[0]
                 bbox[3] += bbox[1]
-                label_idx = self.label_map[obj['category_id']] - 1
+                label_idx = self.label_map[obj['category_id']]
                 final_box = list(np.array(bbox)/scale)
                 final_box.append(label_idx)
                 res += [final_box]  # [xmin, ymin, xmax, ymax, label_idx]
