f11e1085b942b1101ca1bc85192fad9134d3a5c0
AltTeter <200351p@ugs.kochi-tech.ac.jp>
2019-11-03 00:58:07 +0900

    設定を追加

--- data/config.py
+++ data/config.py
@@ -42,9 +42,9 @@ coco = {
 }
 
 ep_coco = {
-    'num_classes': 21,
-    'lr_steps': (280000, 360000, 400000),
-    'max_iter': 400000,
+    'num_classes': 2,
+    'lr_steps': (1000, 2000, 3000),
+    'max_iter': 4000,
     'feature_maps': [38, 19, 10, 5, 3, 1],
     'min_dim': 300,
     'steps': [8, 16, 32, 64, 100, 300],
@@ -53,5 +53,20 @@ ep_coco = {
     'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
     'variance': [0.1, 0.2],
     'clip': True,
-    'name': 'COCO',
+    'name': 'EP_COCO',
+}
+
+fine_tune_ep = {
+    'num_classes': 21,
+    'lr_steps': (1000, 2000, 3000),
+    'max_iter': 4000,
+    'feature_maps': [38, 19, 10, 5, 3, 1],
+    'min_dim': 300,
+    'steps': [8, 16, 32, 64, 100, 300],
+    'min_sizes': [21, 45, 99, 153, 207, 261],
+    'max_sizes': [45, 99, 153, 207, 261, 315],
+    'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
+    'variance': [0.1, 0.2],
+    'clip': True,
+    'name': 'FINE_TUNE_EP',
 }
